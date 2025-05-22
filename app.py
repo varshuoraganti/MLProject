71e5b242acef129bd533f3e4082b61a0bbd69c8a
varshuoraganti <varshuoraganti28@gmail.com>
2025-05-22 22:13:25 +0530

    Update app.py

--- app.py
+++ app.py
@@ -333,12 +333,4 @@ if input_df is not None and models_loaded and selected_model:
                         st.plotly_chart(fig, use_container_width=True)
 
                         csv = processed_df.to_csv(index=False)
-                        st.download_button(
-                            label="💾 Download Predictions as CSV",
-                            data=csv,
-                            file_name="sleep_predictions.csv",
-                            mime="text/csv",
-                        )
-
-                    else:
-                        st.error("⚠️ Prediction")
+                        st.download_button
